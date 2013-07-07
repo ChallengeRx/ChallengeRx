88f887a4265945bb81887893e5cc2eb9b964dbf3
Jarrod Johnson <johnson.jarrod@gmail.com>
2013-07-07 18:58:28 -0400

    Initial website setup

---
+++ Source/ChallengeRx.Web/App_Start/AuthConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using ChallengeRx.Web.Models;
+
+namespace ChallengeRx.Web
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
+            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
+
+            //OAuthWebSecurity.RegisterMicrosoftClient(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //OAuthWebSecurity.RegisterTwitterClient(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            OAuthWebSecurity.RegisterFacebookClient(
+                appId: "539481839422991",
+                appSecret: "639db719bcfd3a56db777628731fef68");
+
+            OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
